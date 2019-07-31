3c45d19233b70d4cefe0e6f493fc273e74436bde
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-07-31 17:58:19 +0000

    Deploy zlocate/adblock-custom-list to github.com/zlocate/adblock-custom-list.git:gh-pages

--- script.sh
+++ script.sh
@@ -1,1 +1,6 @@
-aria2c -i sources.txt -o result.txt
+#/bin/sh
+aria2c --continue=true -i sources.txt -d tmp && \
+awk '{sub(/^127.0.0.1/, "0.0.0.0")} /^0.0.0.0/' ./tmp/*.txt > ./all.txt && \
+awk '{sub(/\r$/,"");print $1,$2}' ./all.txt | sort -u --parallel 8 > ./hosts.deny && \
+rm -rf ./tmp/* all.txt
+
